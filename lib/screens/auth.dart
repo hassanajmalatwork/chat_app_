7a25f65ca3ba8795e8cee76d35472d7bba1c6942
hassanajmalatwork <138983262+hassanajmalatwork@users.noreply.github.com>
2023-09-02 12:56:15 +0500

    added sign in and login and sign out method

--- lib/screens/auth.dart
+++ lib/screens/auth.dart
@@ -1,4 +1,8 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+final _firebase = FirebaseAuth.instance;
 
 class AuthScreen extends StatefulWidget {
   const AuthScreen({super.key});
@@ -9,6 +13,36 @@ class AuthScreen extends StatefulWidget {
 }
 
 class _AuthScreenState extends State<AuthScreen> {
+  final _formkey = GlobalKey<FormState>();
+  var _isenteredemail = '';
+  var _isenteredPassword = '';
+  var _islogin = true;
+  void _submit() async {
+    final isValid = _formkey.currentState!.validate();
+
+    if (!isValid) {
+      return;
+    }
+    _formkey.currentState!.save();
+    try {
+      if (_islogin) {
+        final userCredentials = await _firebase.signInWithEmailAndPassword(
+            email: _isenteredemail, password: _isenteredPassword);
+      } else {
+        final userCredentials = await _firebase.createUserWithEmailAndPassword(
+            email: _isenteredemail, password: _isenteredPassword);
+      }
+    } on FirebaseAuthException catch (error) {
+      if (error.code == 'Email-already-in-use') {
+        //...
+      }
+      ScaffoldMessenger.of(context).clearSnackBars();
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+        content: Text(error.message ?? 'Authentication failed'),
+      ));
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -30,6 +64,7 @@ class _AuthScreenState extends State<AuthScreen> {
                     child: Padding(
                         padding: const EdgeInsets.all(16),
                         child: Form(
+                          key: _formkey,
                           child: Column(
                             mainAxisSize: MainAxisSize.min,
                             children: [
@@ -38,11 +73,51 @@ class _AuthScreenState extends State<AuthScreen> {
                                 keyboardType: TextInputType.emailAddress,
                                 autocorrect: false,
                                 textCapitalization: TextCapitalization.none,
+                                validator: (value) {
+                                  if (value == null ||
+                                      value.trim().isEmpty ||
+                                      !value.contains('@')) {
+                                    return 'Please enter a valid email adress';
+                                  }
+                                },
+                                onSaved: (value) {
+                                  _isenteredemail = value!;
+                                },
                               ),
                               TextFormField(
                                 decoration:
                                     InputDecoration(labelText: 'Password'),
                                 obscureText: true,
+                                validator: (value) {
+                                  if (value == null ||
+                                      value.trim().length < 6) {
+                                    return 'Password must be 6 character long';
+                                  }
+                                },
+                                onSaved: (value) {
+                                  _isenteredPassword = value!;
+                                },
+                              ),
+                              const SizedBox(
+                                height: 12,
+                              ),
+                              ElevatedButton(
+                                onPressed: _submit,
+                                style: ElevatedButton.styleFrom(
+                                    backgroundColor: Theme.of(context)
+                                        .colorScheme
+                                        .primaryContainer),
+                                child: Text(_islogin ? 'Login' : 'Sign up'),
+                              ),
+                              TextButton(
+                                onPressed: () {
+                                  setState(() {
+                                    _islogin = !_islogin;
+                                  });
+                                },
+                                child: Text(_islogin
+                                    ? 'Create an account'
+                                    : 'I already have an account'),
                               )
                             ],
                           ),
